ed12bddaf1803c3f87459f0e19aa81aebfa4eebe
isshe <i.sshe@foxmail.com>
2016-01-02 10:27:54 +0800

    2016.1.2

---
+++ calculator/calculator.c
@@ -0,0 +1,65 @@
+/*==============================================================================\
+* Copyright(C)2016 Chudai.
+*
+* File name    : calculator.c
+* Version      : v1.0.0
+* Author       : 初代
+* Date         : 2016/01/02
+* Description  :
+* Function list: 1.
+*                2.
+*                3.
+* History      :
+\*==============================================================================*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+     double num1 = 0.0;
+     double num2 = 0.0;
+     char   opera ;
+     double result = 0.0;
+
+     printf("Usage: numa /*+- numb\n");
+     while (1)
+     {
+         result = 0.0;
+         printf("please enter: \n");
+         scanf ("%lf %c %lf", &num1, &opera, &num2);
+
+         switch(opera)
+         {
+             case '+':
+                 result = num1 + num2;
+                 break;
+
+             case '-':
+                 result = num1 - num2;
+                 break;
+
+             case '*':
+                 result = num1 * num2;
+                 break;
+
+             case '/':
+                 if (num2 == 0)
+                 {
+                      printf("error: numb = 0!\n");
+                      break;
+                 }
+                 result = num1 / num2;
+                 break;
+
+             default :
+                 break;
+         }//switch()
+
+         printf("=%lf\n", result);
+     }//while(1)
+
+     exit(0);
+}
